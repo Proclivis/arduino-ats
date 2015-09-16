f80a068d8926be87993149ae105b2dd13ed9eb43
Michael Jones <mjones@linear.com>
2015-09-16 08:41:39 -0600

    Update by comparing with arduino-1.6.5-r5 Change ttyUSB0 to ttyACM0 Add -D to Makefile for Mega 2560 (now default)

--- _arduino/w5100.h
+++ _arduino/w5100.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010 by Cristian Maglie <c.maglie@bug.st>
+ * Copyright (c) 2010 by Arduino LLC. All rights reserved.
  *
  * This file is free software; you can redistribute it and/or modify
  * it under the terms of either the GNU General Public License version 2
@@ -13,21 +13,21 @@
 #include <avr/pgmspace.h>
 
 #if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-inline static void initSS()    { DDRB  |=  _BV(4); };
-inline static void setSS()     { PORTB &= ~_BV(4); };
-inline static void resetSS()   { PORTB |=  _BV(4); };
+  inline static void initSS()    { DDRB  |=  _BV(4); };
+  inline static void setSS()     { PORTB &= ~_BV(4); };
+  inline static void resetSS()   { PORTB |=  _BV(4); };
 #elif defined(__AVR_ATmega32U4__)
-inline static void initSS()    { DDRB  |=  _BV(6); };
-inline static void setSS()     { PORTB &= ~_BV(6); };
-inline static void resetSS()   { PORTB |=  _BV(6); };
+  inline static void initSS()    { DDRB  |=  _BV(6); };
+  inline static void setSS()     { PORTB &= ~_BV(6); };
+  inline static void resetSS()   { PORTB |=  _BV(6); }; 
 #elif defined(__AVR_AT90USB1286__) || defined(__AVR_AT90USB646__) || defined(__AVR_AT90USB162__)
-inline static void initSS()    { DDRB  |=  _BV(0); };
-inline static void setSS()     { PORTB &= ~_BV(0); };
-inline static void resetSS()   { PORTB |=  _BV(0); };
+  inline static void initSS()    { DDRB  |=  _BV(0); };
+  inline static void setSS()     { PORTB &= ~_BV(0); };
+  inline static void resetSS()   { PORTB |=  _BV(0); }; 
 #else
-inline static void initSS()    { DDRB  |=  _BV(2); };
-inline static void setSS()     { PORTB &= ~_BV(2); };
-inline static void resetSS()   { PORTB |=  _BV(2); };
+  inline static void initSS()    { DDRB  |=  _BV(2); };
+  inline static void setSS()     { PORTB &= ~_BV(2); };
+  inline static void resetSS()   { PORTB |=  _BV(2); };
 #endif
 
 #endif
--- _arduino/wiring_analog.c
+++ _arduino/wiring_analog.c
@@ -41,7 +41,12 @@ int analogRead(uint8_t pin)
 {
 	uint8_t low, high;
 
-#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+#if defined(analogPinToChannel)
+#if defined(__AVR_ATmega32U4__)
+	if (pin >= 18) pin -= 18; // allow for channel or pin numbers
+#endif
+	pin = analogPinToChannel(pin);
+#elif defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
 	if (pin >= 54) pin -= 54; // allow for channel or pin numbers
 #elif defined(__AVR_ATmega32U4__)
 	if (pin >= 18) pin -= 18; // allow for channel or pin numbers
@@ -157,6 +162,14 @@ void analogWrite(uint8_t pin, int val)
 				// connect pwm to pin on timer 1, channel B
 				sbi(TCCR1A, COM1B1);
 				OCR1B = val; // set pwm duty
+				break;
+			#endif
+
+			#if defined(TCCR1A) && defined(COM1C1)
+			case TIMER1C:
+				// connect pwm to pin on timer 1, channel B
+				sbi(TCCR1A, COM1C1);
+				OCR1C = val; // set pwm duty
 				break;
 			#endif
 
